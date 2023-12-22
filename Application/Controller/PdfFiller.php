1eb12822446332c17413687997ea3a488ff26963
adrien <walczak.adrien.pro@gmail.com>
2023-12-22 10:15:49 +0100

    fix bugs

--- Application/Controller/PdfFiller.php
+++ Application/Controller/PdfFiller.php
@@ -3,51 +3,67 @@
 namespace Application\Controller;
 
 use DateTime;
+use Exception;
 use mikehaertl\pdftk\Pdf;
+
 class PdfFiller
 {
-    public function validate($model, $client)
+    public static $TYPE_INDIVIDUAL = "particulier";
+    public static $TYPE_SOCIETY = "entreprise";
+    public static $DATE_FORMAT = 'Y-m-d';
+    public static function allTypes(): array
     {
-        $FILLED = [];
+        return [
+            self::$TYPE_INDIVIDUAL =>  'ressources/cerfa_particulier.pdf.pdf',
+            self::$TYPE_SOCIETY => 'ressources/cerfa_entreprise.pdf.pdf',
+        ];
+    }
+    public function validate($client)
+    {
+        var_dump($client["type_cerfa"]);
+        if (!in_array($client["type_cerfa"], array_keys(self::allTypes()))) {
+
+            throw new \Exception("incompatible type for field 'type'");
+        }
+        $result = [];
+        $model = json_decode(file_get_contents(__DIR__.'/../Json/data.json'), true);
+
         foreach($model as $field => $rules)
         {
-            $value = @$client[$field];
-            if ($rules['mandatory'] === true && !$value) return "missing field '$field'";
-            if (is_array($rules['mandatory'])) {
-                foreach ($rules['mandatory'] as $subfield => $subvalues) {
-                    foreach ($subvalues as $subvalue) {
-                        if (!isset($value) && $subvalue === $client[$subfield]) return "missing field '$field'";
-                    }
-                }
+            $value = $this->{$field};
+            $mandatory = isset($rules['mandatory']) && $rules['mandatory'] === true && !$value;
+            $dependency = isset($rules['dependency']) && (in_array($client[$rules['dependency']['field']], array_keys($rules['dependency']['values'])) && !$value);
+            if ($mandatory || $dependency) {
+                throw new \Exception("missing field '$field'");
             }
             if (isset($value)) {
                 if ($rules['type'] === 'date' && !$this->isValidDate($value)) {
-                    return "incompatible date format for field '$field'";
+                    throw new \Exception("incompatible date format for field '$field'");
                 }
                 if ($rules['type'] !== 'date' && gettype($value) !== $rules['type']) {
-                    return "incompatible type for field '$field'";
+                    throw new \Exception("incompatible type for field '$field'");
                 }
                 if (isset($rules['dependency'])) {
                     $dependency = $rules['dependency']['field'];
-                    foreach ($rules['dependency']['values'][$client[$dependency]] as $subfield => $subvalue) {
-                        if ($rules['type'] === 'date') {
-                            $FILLED[$subfield] = DateTime::createFromFormat('Y-m-d', $value)->format($subvalue);
-                        } else {
-                            $FILLED[$subfield] = $subvalue;
+                    if (isset($rules['dependency']['values'][$client[$dependency]]))
+                        foreach ($rules['dependency']['values'][$client[$dependency]] as $subfield => $subvalue) {
+                            if ($rules['type'] === 'date') {
+                                $result[$subfield] = DateTime::createFromFormat(self::$DATE_FORMAT, $value)->format($subvalue);
+                            } else {
+                                $result[$subfield] = $subvalue;
+                            }
                         }
-                    }
                 } else {
-                    $FILLED[$rules['field']] = $value;
+                    $result[$rules['field']] = $value;
                 }
             }
         }
-        return $FILLED;
+        return $result;
     }
     public static function createPdf()
     {
         $json= json_decode(file_get_contents(__DIR__.'/../Json/data.json'), true);
         $pdf = new Pdf('ressources/cerfa_entreprise.pdf');
-        var_dump(__DIR__);
         $result = $pdf->fillForm($json)
             ->needAppearances()
             ->saveAs('ressources/filled.pdf');
@@ -67,168 +83,8 @@ class PdfFiller
 
     public function testData()
     {
-        $model = json_decode('{
-  "asso_name":{
-    "type": "string",
-    "mandatory": true,
-    "field": "a2"
-  },
-  "asso_siren":{
-    "type": "string",
-    "mandatory": true,
-    "field": "a4"
-  },
-  "asso_streetNumber":{
-  "type": "string",
-  "mandatory": false,
-  "field": "a5"
-},
-  "asso_street":{
-    "type": "string",
-    "mandatory": true,
-    "field": "a6"
-  },
-  "asso_city":{
-    "type": "string",
-    "mandatory": false,
-    "field": "a6"
-  },
-  "asso_postcode":{
-    "type": "string",
-    "mandatory": false,
-    "field": "a8"
-  },
-  "asso_country":{
-    "type": "string",
-    "mandatory": false,
-    "field": "a9"
-  },
-  "asso_type": {
-    "type": "string",
-    "mandatory": true,
-    "dependency": {
-      "field": "type",
-      "values": {
-        "LOI1901": {
-          "CAC0": 1,
-          "CAC1": 1
-        },
-        "FRUP": {
-          "CAC0": 2,
-          "CAC1": 1
-        },
-        "FRUP_MOZEL": {
-          "CAC0": 2,
-          "CAC1": 1
-        },
-        "FUNIV": {
-          "CAC0": 3,
-          "CAC1": 1
-        },
-        "FENT": {
-          "CAC0": 4,
-          "CAC1": 1
-        },
-        "MDF": {
-          "CAC0": 5,
-          "CAC1": 1
-        },
-        "OSBL": {
-          "CAC0": 6,
-          "CAC1": 1
-        },
-        "AUTRES": {
-          "CAC0": 7,
-          "CAC1": 1
-        },
-        "ASS_CULT": {
-          "CAC2": 1
-        },
-        "ENSSUP": {
-          "CAC3": 1
-        },
-        "ENSSUPCONSULAIRE": {
-          "CAC4": 1
-        },
-        "SOPP": {
-          "CAC5": 1
-        },
-        "ENSSUPCINE": {
-          "CAC6": 1
-        },
-        "ASS_OPP": {
-          "CAC7": 1
-        },
-        "MECENE": {
-          "CAC8": 1
-        },
-        "ETATACT": {
-          "CAC9": 1
-        },
-        "SNPMCOM": {
-          "CAC10": 1
-        },
-        "SNPMMUSIC": {
-          "CAC11": 1
-        },
-        "SNPMCOM": {
-          "CAC12": 1
-        },
-        "ONDPAT": {
-          "CAC13": 1
-        },
-        "FONDDOTATION": {
-          "CAC14": 1
-        },
-        "FINANCEENT": {
-          "CAC15": 1
-        },
-        "FEDERER": {
-          "CAC16": 1
-        },
-        "SAUVEGARDE": {
-          "CAC17": 1
-        },
-        "UE": {
-          "CAC18": 1
-        }
-      }
-    }
-  },
-  "type": "date",
-  "mandatory": {
-    "type": ["FRUP", "FRUP_MOZEL", "FUNIV", "SOPP", "FONDPAT","FINANCEENT","FEDERER"]
-  },
-  "dependency": {
-    "field": "type",
-    "values": {
-      "FRUP": {
-        "d12": "d/m/y",
-        "d13": "d/m/y"
-      },
-      "FRUP_MOZEL": {
-        "d14": "d/m/y"
-      },
-      "FUNIV": {
-        "d14": "d/m/y"
-      },
-      "SOPP": {
-        "a14": "d/m/y"
-      },
-      "FONDPAT": {
-        "a15": "d/m/y"
-      },
-      "FINANCEENT": {
-        "a15": "d/m/y"
-      },
-      "FEDERER": {
-        "a15": "d/m/y"
-      }
-    }
-  }
-}', true);
-
-                $client = json_decode('{
+        $client = json_decode('{
+            "type_cerfa" : "entreprise",
             "asso_siren": "SPA",
             "asso_name": "LA SPA",
             "asso_street": "Paris",
@@ -236,15 +92,17 @@ class PdfFiller
             "date": "2023-01-01"
         }', true);
                 $client2 = json_decode('{
+            "type_cerfa" : "entreprise",
             "asso_siren": "SPA",
             "asso_name": "LA SPA",
             "asso_street": "Paris",
             "asso_type": "FRUP",
             "date": "2023-01-01"
         }', true);
-                echo json_encode( $this->validate($model, $client) ) ." >> Done". PHP_EOL;
-                echo json_encode( $this->validate($model, $client2) ) ." >> Done". PHP_EOL;
+                echo json_encode( $this->validate($client) ) ." >> Done". PHP_EOL;
+                echo json_encode( $this->validate($client2) ) ." >> Done". PHP_EOL;
                 $client = json_decode('{
+            "type_cerfa" : "entreprise",
             "asso_siren": "SPA",
             "asso_name": "LA SPA",
             "asso_street": "Paris",
@@ -252,6 +110,7 @@ class PdfFiller
             "date": "2023-01-01"
         }', true);
                 $client2 = json_decode('{
+            "type_cerfa" : "entreprise",
             "asso_siren": "SPA",
             "asso_name": "LA SPA",
             "asso_street": "Paris",
@@ -259,6 +118,7 @@ class PdfFiller
             "date": "2023-01-01"
         }', true);
                 $client3 = json_decode('{
+            "type_cerfa" : "entreprise",
             "asso_siren": "SPA",
             "asso_name": "LA SPA",
             "asso_street": "Paris",
@@ -267,9 +127,38 @@ class PdfFiller
         }', true);
 
 
-        echo json_encode( $this->validate($model, $client) ) ." >> Done". PHP_EOL;
-        echo json_encode( $this->validate($model, $client2) ) ." >> Done". PHP_EOL;
-        echo json_encode( $this->validate($model, $client3) ) ." >> Done". PHP_EOL;
+        echo json_encode( $this->validate($client) ) ." >> Done". PHP_EOL;
+        echo json_encode( $this->validate($client2) ) ." >> Done". PHP_EOL;
+        echo json_encode( $this->validate($client3) ) ." >> Done". PHP_EOL;
+    }
+    private function createSignature(string $id, string $base64Signature)
+    {
+        $path = "Signatures/signature_{$id}";
+        $png = $path .'.png';
+        $pdf = $path .'.pdf';
+        file_put_contents($png, $base64Signature);
+        $tcpdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        $tcpdf->AddPage(); // page blank
+        $tcpdf->AddPage();
+        $tcpdf->Image($png,134,216,50,20,'PNG'); // @TODO How to keep ratio ?
+        $tcpdf->Output('/home/runcloud/webapps/cerfa-generator/'. $pdf, 'F');
+        return $pdf;
+    }
+    public function generatePDF()
+    {
+        $id = uniqid();
+        $signature = $this->createSignature($id, base64_decode($this->values()['signature']));
+        $filename = "CerfaReceipt{$id}.pdf";
+        $template = new Pdf('ressources/cerfa_entreprise.pdf');
+        $result = $template->fillForm($this->values());
+        $template = new Pdf($template);
+        $result = $template->flatten() // to compress
+        ->multistamp($signature) // to add signature
+        ->saveAs('PdfGenerated/' . $filename);
+        if ($result === false) {
+            throw new \Exception($template->getError());
+        }
+        $this->file = $filename;
     }
     public function isValidDate($date, $format = 'Y-m-d'): bool
     {
